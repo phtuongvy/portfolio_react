0f80732d0a1e14dc4ba5255b8926de792a2ed893
phtuongvy <Tuong-Vy.Pham-Huynh@etu.univ-smb.fr>
2024-04-21 19:40:54 +0200

    add sidebar component with logo and navigation menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,54 @@
 import React from "react";
 import "./sidebar.css";
+import Logo from "../../assets/wolf.png";
 
 
 const Sidebar = () => {
     return (
-        <div>
-            Sidebar
-        </div>
+        <aside className="aside">
+            <a href="#home" className="nav__logo">
+                <img src={Logo} alt="" />
+            </a>
+
+            {/* nav menu sidebar */}
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                                <i className="icon-home"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                                <i className="icon-user"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#skills" className="nav__link">
+                                <i className="icon-briefcase"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#resume" className="nav__link">
+                                <i className="icon-graduation"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                                <i className="icon-layers"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                                <i className="icon-bubble"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </aside>
     )
 
 }
