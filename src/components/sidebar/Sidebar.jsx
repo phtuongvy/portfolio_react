0176b149a9a40d306fb3ba15643440ac6751d375
phtuongvy <Tuong-Vy.Pham-Huynh@etu.univ-smb.fr>
2024-04-21 18:50:49 +0200

    add components + modify index.html + add variables css to index.css

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sidebar.css";
+
+
+const Sidebar = () => {
+    return (
+        <div>
+            Sidebar
+        </div>
+    )
+
+}
+
+export default Sidebar;
